ee98c9e353038158c08fcb1314e5ab5af9420f1f
JaeYooooon <dhy04050@naver.com>
2023-05-16 13:58:17 +0900

    log log back 을 사용하여 로그 남기기 gitignore .log 추가

--- src/main/java/zerobase/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/zerobase/dividend/scheduler/ScraperScheduler.java
@@ -61,6 +61,7 @@ public class ScraperScheduler {
                         boolean exists = this.dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
                         if(!exists){
                             this.dividendRepository.save(e);
+                            log.info("insert new dividend -> " + e.toString());
                         }
                     });
             try {
